d25ebb80b1d69ddc0f9b578361a1beedbbaa434d
viet21092003 <21124548@st.hcmuaf.edu.vn>
2024-05-28 19:56:20 +0700

    fix search and filter feature

--- src/main/java/com/lamnguyen/chat_online/controllers/MessageController.java
+++ src/main/java/com/lamnguyen/chat_online/controllers/MessageController.java
@@ -4,6 +4,8 @@ import com.google.firebase.database.*;
 import com.lamnguyen.chat_online.enums.Status;
 import com.lamnguyen.chat_online.model.Message;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +15,14 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api")
 public class MessageController {
+    private static final Logger log = LoggerFactory.getLogger(MessageController.class);
     @Autowired
     private DatabaseReference reference;
     @Autowired
@@ -51,7 +55,7 @@ public class MessageController {
     public String sendMessage(@PathVariable("type") String type, @PathVariable("id") String roomId, @RequestBody @Valid Message message) throws ExecutionException, InterruptedException {
         LocalDateTime now = LocalDateTime.now();
         String key = String.valueOf(now.toInstant(ZoneOffset.UTC).toEpochMilli());
-        message.setStatus(Status.SENT);
+        message.setStatus("SENT");
         message.setTime(formatter.format(now));
         CompletableFuture<String> future = new CompletableFuture<>();
         reference.child(type).child(roomId).child(key)
@@ -68,7 +72,7 @@ public class MessageController {
             @PathVariable("type") String type,
             @PathVariable("id") String roomId,
             @RequestParam(value = "content", required = false) String content,
-            @RequestParam(value = "status", required = false) Status status,
+            @RequestParam(value = "status", required = false) String status,
             @RequestParam(value = "startTime", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime,
             @RequestParam(value = "endTime", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime) throws ExecutionException, InterruptedException {
 
@@ -79,25 +83,17 @@ public class MessageController {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 List<Message> messages = new ArrayList<>();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Message message = snapshot.getValue(Message.class);
-                    if (message != null) {
-                        boolean matches = true;
 
-                        if (content != null && !message.getMessage().contains(content)) {
-                            matches = false;
-                        }
-                        if (status != null && message.getStatus() != status) {
-                            matches = false;
-                        }
-                        if (startTime != null || endTime != null) {
-                            LocalDateTime messageTime = LocalDateTime.parse(message.getTime(), formatter);
-                            if (startTime != null && messageTime.isBefore(startTime)) {
-                                matches = false;
-                            }
-                            if (endTime != null && messageTime.isAfter(endTime)) {
-                                matches = false;
-                            }
-                        }
+                    Message message = null;
+                    try {
+                        message = snapshot.getValue(Message.class);
+                    } catch (Exception e) {
+                       System.out.println(e);
+                    }
+
+
+                    if (message != null) {
+                        boolean matches = isMatches(message);
 
                         if (matches) {
                             messages.add(message);
@@ -105,6 +101,26 @@ public class MessageController {
                     }
                 }
                 future.complete(messages);
+            }
+
+            private boolean isMatches(Message message) {
+                boolean matches = content == null || message.getMessage().toLowerCase().contains(content.toLowerCase());
+
+                if (message.getStatus().equals("REDEEM")) matches = false;
+
+                if (status != null && !Objects.equals(message.getStatus(), status)) {
+                    matches = false;
+                }
+                if (startTime != null || endTime != null) {
+                    LocalDateTime messageTime = LocalDateTime.parse(message.getTime(), formatter);
+                    if (startTime != null && messageTime.isBefore(startTime)) {
+                        matches = false;
+                    }
+                    if (endTime != null && messageTime.isAfter(endTime)) {
+                        matches = false;
+                    }
+                }
+                return matches;
             }
 
             @Override
